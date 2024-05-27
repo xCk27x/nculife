7e113b68a8e3cca8ac6f1a1335ccc3b874f6863b
xck27x <pdes0722@gmail.com>
2024-05-27 21:01:00 +0800

    5/27 pages/nculife/group/club update

--- pages/nculife/group/club/index.vue
+++ pages/nculife/group/club/index.vue
@@ -1,5 +1,46 @@
 <template>
-  <div>
-    
+  <div class=" w-screen h-screen pt-16 flex justify-center items-center">
+    <div class="px-14 h-full w-clamp-10rem-60vw-20rem flex items-center">
+      <div class="h-3/5 w-full bg-slate-300 p-[5%]">
+        <ul class="book-container h-full">
+          <li><NuxtLink to="/nculife/group/intro/dance">熱舞社</NuxtLink></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+
+
+.w-clamp-10rem-60vw-20rem {
+  width: clamp(5rem, 80vw, 140vh);
+}
+
+ul.book-container {
+  display: flex;
+  justify-content: space-around;
+  background-color: white;
+  padding: 2%;
+
+  & li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 12%;
+    height: 100%;
+    background-color: rgb(203 213 225);
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    writing-mode: vertical-rl;
+    text-orientation: upright;
+    font-size: 2rem;
+  }
+}
+</style>
